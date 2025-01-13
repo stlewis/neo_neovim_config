916b1c7e4aa63068408d4e932f98ddef523a6fcd
Steve Lewis <steve@decodingsteve.com>
2025-01-13 13:55:05 -0800

    Neotest plugin

---
+++ lua/stlewis/plugins/nvim-neoest.lua
@@ -0,0 +1,38 @@
+-- Test Runners
+vim.api.nvim_create_user_command("RunNearestSpec", function()
+  vim.cmd("wa")
+  require("neotest").run.run()
+  require("neotest").output_panel.open()
+end, {})
+
+vim.api.nvim_create_user_command("RunFileSpec", function()
+  vim.cmd("wa")
+  require("neotest").run.run(vim.fn.expand("%"))
+  require("neotest").output_panel.open()
+end, {})
+
+-- Keymaps
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>ns", ":RunNearestSpec<CR>", { silent = true })
+keymap.set("n", "<leader>as", ":RunFileSpec<CR>", { silent = true })
+
+return {
+  "nvim-neotest/neotest",
+  lazy = true,
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "olimorris/neotest-rspec",
+  },
+
+  config = function()
+    require("neotest").setup({
+      adapters = {
+        require("neotest-rspec")
+      }
+    })
+  end,
+}
