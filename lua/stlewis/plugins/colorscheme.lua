c8948d1b83e0ae2e1a0f2ff410e7e417c76fd2e1
Steve Lewis <steve@decodingsteve.com>
2025-01-14 15:31:47 -0800

    Themify plugin for colorscheme management

--- lua/stlewis/plugins/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "rafi/awesome-vim-colorschemes",
-  priority = 1000,
-  config = function()
-    vim.cmd("colorscheme focuspoint")
-  end
-}
---
+++ lua/stlewis/plugins/themify.lua
@@ -0,0 +1,9 @@
+return {
+  "LmanTW/themify.nvim",
+  lazy = false,
+  priority = 999,
+  config = {
+    "rafi/awesome-vim-colorschemes",
+    "folke/tokyonight.nvim",
+  }
+}
